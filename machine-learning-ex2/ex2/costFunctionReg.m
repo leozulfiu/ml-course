cc0d5f227e6442cc5feb9337cfc5d4f116dbe3b6
Leo Zulfiu <leo.zulfiu@gmx.ch>
2016-10-10 12:59:29 +0200

    finish exercise 2

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,9 +17,15 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X*theta);
+shifted_theta = theta(2:size(theta));
+theta_reg = [0;shifted_theta]; % add theta0 to the feature vector
 
+J = ((1/m) * (-y' * log(h) - (1 - y)' * log(1 - h)));
+J = J + (lambda/(2*m)) * theta_reg' * theta_reg;
 
-
+grad(1) = (1/m) * (X(:,1))' * (h - y);
+grad(2:size(grad)) = ((1/m) * (X(:,[2:end]))' * (h - y)) + (lambda/m)*shifted_theta;
 
 
 % =============================================================
