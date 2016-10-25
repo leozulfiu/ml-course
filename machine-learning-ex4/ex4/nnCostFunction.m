1390bc10415afc1d48029404948bf8ea8497dc60
Leo Zulfiu <leo.zulfiu@gmx.ch>
2016-10-25 16:35:17 +0200

    finish exercise 4 which is able to train a neural network

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -1,4 +1,4 @@
-function [J grad] = nnCostFunction(nn_params, ...
+function [J, grad] = nnCostFunction(nn_params, ...
                                    input_layer_size, ...
                                    hidden_layer_size, ...
                                    num_labels, ...
@@ -62,23 +62,45 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% prepare y matrices
+I = eye(num_labels);
+Y = zeros(m, num_labels);
+for i=1:m
+  Y(i, :)= I(y(i), :);
+end
 
+% feedforward
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
 
+a2 = [ones(m, 1) sigmoid(z2)];
+z3 = a2 * Theta2';
 
+a3 = sigmoid(z3);
+h = a3;
 
+% calculate J
+J = (1/m) * sum(sum(-Y .* log(h) - (1-Y) .* log(1-h)));
 
+regularization = (lambda/(2*m)) * ( ...
+      (sum(sum(Theta1(:, 2:end) .^ 2))) + ...
+      (sum(sum(Theta2(:, 2:end) .^ 2))) ...
+);
 
+J = J + regularization;
 
+% backpropagation
+sigma3 = a3 - Y;
+sigma2 = sigma3 * Theta2(:, 2:end) .* sigmoidGradient(z2);
 
+delta_1 = (sigma2' * a1);
+delta_2 = (sigma3' * a2);
 
+th1_regularization = (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:, 2:end)];
+th2_regularization = (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:, 2:end)];
 
-
-
-
-
-
-
-
+Theta1_grad = delta_1./m + th1_regularization;
+Theta2_grad = delta_2./m + th2_regularization;
 
 % -------------------------------------------------------------
 
